21cc80f4a2b16cbed6d43d92be3b96f4fdc71c63
Сулайманова Сайкал <sulaymanovasaykal2003gmail.com>
2025-01-11 09:22:08 +0600

    handle cant read ru command

--- backend/telegram_bot.py
+++ backend/telegram_bot.py
@@ -18,6 +18,7 @@ from telegram_bot.language.language_dict import translations
 from telegram_bot.language.service.get_translation import get_translation
 def main():
     lang_translations = translations['en']
+    ru_translations = translations['ru']
     application = Application.builder().token(BOT_TOKEN).build()
     application.add_handler(MessageHandler(filters.PHOTO, handle_img))
     application.add_handler(CommandHandler(lang_translations['start_command'], start))
@@ -26,6 +27,10 @@ def main():
     application.add_handler(MessageHandler(filters.Regex(lang_translations["choose_language_command"]), choose_lang)) #for ru not work
     application.add_handler(MessageHandler(filters.Regex(lang_translations["help_text"]), help_command))
     application.add_handler(MessageHandler(filters.Regex(lang_translations["filter_checks_command"]), filter_receipt))
+
+    application.add_handler(MessageHandler(filters.Regex(ru_translations["choose_language_command"]), choose_lang)) #for ru not work
+    application.add_handler(MessageHandler(filters.Regex(ru_translations["help_text"]), help_command))
+    application.add_handler(MessageHandler(filters.Regex(ru_translations["filter_checks_command"]), filter_receipt))
     application.add_handler(CallbackQueryHandler(callback_query_handler))
     application.run_polling()
 
